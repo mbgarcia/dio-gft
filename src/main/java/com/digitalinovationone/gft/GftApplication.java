6a65e6c10444da935b4fb55e489ad4b317bd8a78
Marcelo Garcia <mbgarcia_pa@yahoo.com.br>
2021-10-05 15:16:53 -0300

    #001 - Initial commit

---
+++ src/main/java/com/digitalinovationone/gft/GftApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinovationone.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GftApplication.class, args);
+	}
+
+}
